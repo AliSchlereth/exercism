12ca9e6c0f3c6b8bc038894cbfcb826780447976
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-08 16:12:45 -0700

    Complete Leap.js

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,16 +1,17 @@
-var Year = function() {};
+var Year = function(input) {
+  var year = input;
 
-
-Year.prototype.isLeap = function(year) {
-  if (year % 4 === 0 && year % 400 === 0){
+Year.prototype.isLeap = function() {
+  if ((year % 4 === 0) && (year % 400 === 0)){
     return true;
-  } else if (year % 4 === 0 && year % 100 === 0) {
+  } else if ((year % 4 === 0) && (year % 100 === 0)) {
     return false;
   } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
   }
-}
+};
+};
 
 module.exports = Year;
