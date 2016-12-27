19e485eecb45f48aeae0047e1f4b67fcaa3614db
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-26 19:14:06 -0700

    Hamming JS complete

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,16 @@
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(a, b) {
+  var hammingDistance = 0;
+  if (a.length != b.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
+  for(i = 0; i < a.length; i++) {
+    if (a[i] != b[i]) {
+      hammingDistance++;
+    }
+  }
+  return hammingDistance;
+}
+
+module.exports = Hamming;
