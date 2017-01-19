74d3937b269efd5a33ebf1aaeae4ca38ccb6dd72
Ali Schlereth <aschlere@sbcglobal.net>
2017-01-19 12:28:43 -0700

    Refactor word-count JS

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,21 +1,34 @@
 var Words = function() {};
 
-  Words.prototype.count = function(phrase) {
-    var cleanPunct = phrase.replace(/[-!$%^&*()_+|~=`{}\[\]:";<>?.¡¿@\/]/g, '')
+  var cleanPhrase = function(phrase) {
+    var cleanPunct = phrase.replace(/[-!$%^&*()_+|~=`{}\[\]:";<>?.¡¿@\/]/g, '');
     var cleanCase = cleanPunct.toLowerCase();
     var cleanNewLine = cleanCase.replace(/\r?\n|\t\r/g, " ");
-    var cleanPhrase = cleanNewLine.replace('\t', ' ').trim();
-    var words = cleanPhrase.split(/[ ,]+/);
+    var cleanWhiteSpace = cleanNewLine.replace('\t', ' ').trim();
+    return cleanWhiteSpace.split(/[ ,]+/);
+  }
+
+  var cleanQuotes = function(word) {
+    if (word.indexOf("'") == 0) {
+      word = word.replace(/[']/g, '');
+    }
+    return word
+  }
+
+  var assignCount = function(word, wordCounts) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1
+    } else {
+      wordCounts[word] = 1
+    }
+  }
+
+  Words.prototype.count = function(phrase) {
+    var words = cleanPhrase(phrase);
     var wordCounts = Object.create(null);
     words.forEach(function(word) {
-      if (word.indexOf("'") == 0) {
-        var word = word.replace(/[']/g, '');
-      }
-      if (wordCounts[word]) {
-        wordCounts[word] += 1
-      } else {
-        wordCounts[word] = 1
-      }
+      word = cleanQuotes(word);
+      assignCount(word, wordCounts);
     });
     return wordCounts
   }
